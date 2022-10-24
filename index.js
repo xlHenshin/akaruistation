8660d0f53c56c9eac5f68397f3cc849297d3e7d6
xlHenshin <andreslayer117@gmail.com>
2022-10-23 21:44:43 -0500

    db working

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+const registerForm = document.getElementById("confirmar")
+
+const enviarInfo = async(email) =>{
+    
+    try {
+        const data = await addDoc(collection(db, "users"),{
+            email
+        });
+
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+registerForm.addEventListener("submit", e =>{
+
+    e.preventDefault();
+
+    const email = registerForm.email.value;
+    console.log("valor email: "+email);
+
+    if (email) {
+        enviarInfo(email);
+        alert("Gracias por registrarte en la beta. Estaremos en contacto contigo :)");
+        registerForm.email.value="";
+    }else{
+        alert("Completa el campo de email")
+    };   
+});
--- utils.js
+++ utils.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDaKCaDbzNkGQN9R2QdOnDUTE4b-lHLGFU",
-    authDomain: "akarui-ad076.firebaseapp.com",
-    projectId: "akarui-ad076",
-    storageBucket: "akarui-ad076.appspot.com",
-    messagingSenderId: "569733627137",
-    appId: "1:569733627137:web:4f8c58aceec6d475186f00",
-    measurementId: "G-9QGL43R11D"
-  };
+  apiKey: "AIzaSyDaKCaDbzNkGQN9R2QdOnDUTE4b-lHLGFU",
+  authDomain: "akarui-ad076.firebaseapp.com",
+  projectId: "akarui-ad076",
+  storageBucket: "akarui-ad076.appspot.com",
+  messagingSenderId: "569733627137",
+  appId: "1:569733627137:web:4f8c58aceec6d475186f00",
+  measurementId: "G-9QGL43R11D"
+};
